fafbbce33bf203f09d51e58bedf702d1c411101f
ameendababat <ameendababat07@gmail.com>
2026-03-24 01:04:25 +0200

    formatting ci

--- src/apikey.test.ts
+++ src/apikey.test.ts
@@ -1,16 +1,16 @@
 import { describe, expect, test } from "vitest";
 
 const person = {
-    isActive: true,
-    age: 32,
+  isActive: true,
+  age: 32,
 };
 
 describe("person", () => {
-    test("person is defined", () => {
+  test("person is defined", () => {
     expect(person).toBeDefined();
-    });
+  });
 
-    test("is active", () => {
+  test("is active", () => {
     expect(person.isActive).toBeTruthy();
-    });
+  });
 });
